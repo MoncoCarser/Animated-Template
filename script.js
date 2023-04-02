83fe2a2a1844c0048bf6acd1d5f37f23c23f2943
MoncoCarser <pjpakkala@gmail.com>
2023-04-02 15:30:34 +0200

    Code reviewed. Tested script with defer

---
+++ script.js
@@ -0,0 +1,6 @@
+  AOS.init({
+    delay: 200, // values from 0 to 3000, with steps of 50ms
+    duration: 1500, // values from 0 to 3000, with steps of 50ms
+    once: false, // whether animation should happen only once - while scrolling
+    mirror: false,  // whether elements should animate out while scrolling
+  });
